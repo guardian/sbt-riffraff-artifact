04aac2363bb5eef5280c13a73855770c04398a36
Akash Askoolum <akash1810@gmail.com>
2021-09-29 11:24:27 +0100

    fix: Improve branch lookup from GitHub Actions
    The branch name within a GitHub Actions environment can be in one of two places:
      - `GITHUB_HEAD_REF` during PR builds
      - `GITHUB_REF` during a build
    
    We read `GITHUB_HEAD_REF` and fallback to `GITHUB_REF`.
    
    Either can be the empty string which is not very useful,
    especially as Riff-Raff uses branch names for continuous and scheduled deployment.
    
    This change guards against this possibility by checking an env var isn't empty and defaults to `"unknown-branch"`.

--- src/main/scala/com/gu/riffraff/artifact/BuildInfo.scala
+++ src/main/scala/com/gu/riffraff/artifact/BuildInfo.scala
@@ -26,7 +26,10 @@ case class BuildInfo(
 object BuildInfo {
 
   val UNKNOWN = "unknown"
+  val UNKNOWN_BRANCH = "unknown-branch"
+
   def env(propName: String): Option[String] = sys.env.get(propName)
+  def envNonEmpty(propName: String): Option[String] = sys.env.get(propName).filter(_.trim.nonEmpty)
 
   val unknown = BuildInfo(
     buildIdentifier = UNKNOWN,
@@ -57,9 +60,9 @@ object BuildInfo {
     // TRAVIS_BRANCH is master when building PRs
     def getTravisBranch: Option[String] = {
       env("TRAVIS_PULL_REQUEST") match {
-        case Some("false") => env("TRAVIS_BRANCH").orElse(Some("unknown-branch"))
+        case Some("false") => env("TRAVIS_BRANCH").orElse(Some(UNKNOWN_BRANCH))
         case Some(i) => Some(s"pr/$i")
-        case None => Some("unknown-branch")
+        case None => Some(UNKNOWN_BRANCH)
       }
     }
 
@@ -142,8 +145,9 @@ object BuildInfo {
 
       // `GITHUB_HEAD_REF` is only set for pull request events and represents the branch name (e.g. `feature-branch-1`).
       // `GITHUB_REF` is the branch or tag ref that triggered the workflow (e.g. `refs/heads/feature-branch-1` or `refs/pull/259/merge` or `refs/heads/main`).
+      // Either can be the empty string ¯\_(ツ)_/¯
       // See https://docs.github.com/en/actions/learn-github-actions/environment-variables
-      ref <- env("GITHUB_HEAD_REF").orElse(env("GITHUB_REF"))
+      ref <- envNonEmpty("GITHUB_HEAD_REF").orElse(envNonEmpty("GITHUB_REF")).orElse(Some(UNKNOWN_BRANCH))
 
       baseUrl <- env("GITHUB_SERVER_URL")
       repo <- env("GITHUB_REPOSITORY")
